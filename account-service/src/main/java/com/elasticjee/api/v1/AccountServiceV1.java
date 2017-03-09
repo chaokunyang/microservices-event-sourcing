146a2e6fc46a1b77015c56e8e785a51ce1ff18fe
chaokunyang <chaokunyang@yahoo.com>
2017-03-10 00:17:54 +0800

    完成账户服务API设计

---
+++ account-service/src/main/java/com/elasticjee/api/v1/AccountServiceV1.java
@@ -0,0 +1,26 @@
+package com.elasticjee.api.v1;
+
+import com.elasticjee.account.Account;
+import com.elasticjee.account.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author chaokunyang
+ * @create 2017/3/10
+ */
+@Service
+public class AccountServiceV1 {
+    @Autowired
+    private AccountRepository accountRepository;
+    @Autowired
+    private OAuth2RestTemplate restTemplate;
+
+    public List<Account> getUserAccount() {
+        List<Account> accounts = null;
+        return accounts;
+    }
+}
