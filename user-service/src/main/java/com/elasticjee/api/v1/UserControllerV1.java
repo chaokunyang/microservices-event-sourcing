51ff7847f443f0ce9367527d0567832cbb7d0de9
chaokunyang <chaokunyang@yahoo.com>
2017-03-13 22:03:00 +0800

    用户服务增加缓存功能，用户服务增加用户CRUD功能

--- user-service/src/main/java/com/elasticjee/api/v1/UserControllerV1.java
+++ user-service/src/main/java/com/elasticjee/api/v1/UserControllerV1.java
@@ -1,17 +1,16 @@
 package com.elasticjee.api.v1;
 
 import com.elasticjee.user.User;
+import jdk.nashorn.internal.runtime.regexp.joni.encoding.ObjPtr;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.Optional;
-
-import static org.apache.coyote.http11.Constants.a;
 
 /**
  * @author yangck
@@ -24,14 +23,45 @@ public class UserControllerV1 {
     private UserServiceV1 userService;
 
     @RequestMapping(path = "/me")
-    public ResponseEntity me(Principal principal) {
+    public ResponseEntity<User> me(Principal principal) {
         User user = null;
         if(principal != null) {
             user = userService.getUserByUsername(principal.getName());
         }
         return Optional.ofNullable(user)
-                .map(a -> new ResponseEntity<User>(a, HttpStatus.OK))
+                .map(a -> new ResponseEntity<>(a, HttpStatus.OK))
                 .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
     }
 
+    @RequestMapping(path = "/users", method = RequestMethod.POST, name = "createUser")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        Assert.notNull(user);
+        return Optional.ofNullable(userService.createUser(user))
+                .map(result -> new ResponseEntity<>(result, HttpStatus.CREATED))
+                .orElse(new ResponseEntity<>(HttpStatus.CONFLICT));
+    }
+
+    @RequestMapping(path = "users/{id}", method = RequestMethod.GET, name = "getUser")
+    public ResponseEntity<User> getUser(@PathVariable("id") Long id) {
+        return Optional.ofNullable(userService.getUserById(id))
+                .map(result -> new ResponseEntity<>(result, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @RequestMapping(path = "users/{id}", method = RequestMethod.PUT, name = "updateUser")
+    public ResponseEntity<User> updateUser(@PathVariable(value = "id") Long id, @RequestBody User user) {
+        Assert.notNull(user);
+        user.setId(id);
+        return Optional.ofNullable(userService.updateUser(id, user))
+                .map(result -> new ResponseEntity<>(result, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @RequestMapping(path = "users/{id}", method = RequestMethod.DELETE,
+            name = "deleteUser")
+    public ResponseEntity deleteUser(@PathVariable("id") Long id) {
+        return Optional.ofNullable(userService.deleteUser(id))
+                .map(result -> new ResponseEntity<>(result, HttpStatus.NO_CONTENT))
+                .orElse(new ResponseEntity<Boolean>(HttpStatus.NOT_FOUND));
+    }
 }
--- user-service/src/main/java/com/elasticjee/api/v1/UserServiceV1.java
+++ user-service/src/main/java/com/elasticjee/api/v1/UserServiceV1.java
@@ -4,6 +4,9 @@ import com.elasticjee.user.User;
 import com.elasticjee.user.UserRepository;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,8 +18,43 @@ public class UserServiceV1 {
     @Autowired
     private UserRepository userRepository;
 
+    @Cacheable(value = "user", key = "#id")
     @HystrixCommand
     public User getUserByUsername(String username) {
         return userRepository.findUserByUsername(username);
     }
+
+    @CacheEvict(value = "user", key = "#user.getId()")
+    public User createUser(User user) {
+        User result = null;
+        if(!userRepository.exists(user.getId())) {
+            result = userRepository.save(user);
+        }
+        return result;
+    }
+
+    @Cacheable(value = "user", key = "#id")
+    public User getUserById(Long id) {
+        return userRepository.findOne(id);
+    }
+
+    @CachePut(value = "user", key = "#id")
+    public User updateUser(Long id, User user) {
+        User result = null;
+        if(userRepository.exists(user.getId())) {
+            result = userRepository.save(user);
+        }
+        return result;
+    }
+
+    @CacheEvict(value = "user", key = "#id")
+    public boolean deleteUser(Long id) {
+        boolean deleted = false;
+        if(userRepository.exists(id)) {
+            userRepository.delete(id);
+            deleted = true;
+        }
+        return deleted;
+    }
 }
+
