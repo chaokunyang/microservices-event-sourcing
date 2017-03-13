ec5290e13ed96969b7e354d056bdef559ec57d86
chaokunyang <chaokunyang@yahoo.com>
2017-03-13 22:52:01 +0800

    实现配置中心，增加配置仓库

---
+++ config-service/src/main/java/com/elasticjee/ConfigServiceApplication.java
@@ -0,0 +1,16 @@
+package com.elasticjee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author chaokunyang
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+}
