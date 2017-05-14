ad068756fab033af7ddcd60853a8096bc2dccaf0
chaokunyang <chaokunyang@yahoo.com>
2017-05-14 22:43:10 +0800

    增加maven构建功能

--- hystrix-dashboard/readme.md
+++ hystrix-dashboard/readme.md
@@ -1,1 +1,4 @@
+# Hystrix Dashboard
+Hystrix Dashboard 负责提供一个面板，用于监控暴露hystrix stream 端点的微服务里面的断路器
+
 1、访问地址：http://localhost:6161/hystrix
--- payment-service/src/main/java/com/timeyang/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/timeyang/PaymentServiceApplication.java
@@ -1,8 +1,17 @@
 package com.timeyang;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * 支付服务
  * @author yangck
  */
+@SpringBootApplication
 public class PaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceApplication.class, args);
+    }
+
 }
